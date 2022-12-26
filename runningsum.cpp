cd0207535cdb689f7c600290a8f3155e04f94890
erzagashi11 <121411956+erzagashi11@users.noreply.github.com>
2022-12-26 03:27:07 +0100

    Add files via upload

---
+++ runningsum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void runningSum(vector<int>& nums) {
+    vector<int> ans;
+    int n = nums.size();
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        count += nums[i];
+        ans.push_back(count);
+    }
+    cout << "The numbers in the vector after running the sum are: " << endl;
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
+}
+void write_vector(vector<int>& V)
+{
+    cout << "The numbers in the vector are: " << endl;
+    for (int i = 0; i < V.size(); i++)
+        cout << V[i] << " ";
+}
+
+int main() {
+
+    int input, n;
+    vector<int> V;
+    cout << "How many numbers do you want to enter:";
+    cin >> n;
+    cout << "Enter your numbers " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        V.push_back(input);
+    }
+
+    write_vector(V);
+    cout << endl;
+    runningSum(V);
+
+
+}
